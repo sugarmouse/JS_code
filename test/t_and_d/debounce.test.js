01154aefdb8fec91aff8917192b5b8ab326e5757
sugarmouse <tanghao19950911@gmail.com>
2022-08-07 00:45:20 +0800

    add test code of debounce and throttle

--- test/t_and_d/debounce.test.js
+++ test/t_and_d/debounce.test.js
@@ -1,7 +1,32 @@
+const debounce = require('../../src/t_and_d/debounce.js')
 
-const  debounce = require ('../../src/t_and_d/debounce.js')
-describe('debounce test',()=>{
-  test("pass", () => {
-    // test code
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+describe('debounce test', () => {
+  test("防抖函数多次在一定时间内多次触发只会执行一次", () => {
+    const mockFn = jest.fn(arg => null)
+    let func = debounce(mockFn, 3)
+    func()
+    func()
+    jest.runAllTimers()
+    expect(mockFn).toHaveBeenCalledOnce()
+  })
+
+  test("防抖函数多次在一定时间内多次触发会重新计时", () => {
+    const mockFn = jest.fn(arg => null)
+    const mockFn2 = jest.fn(arg => null)
+    let func = debounce(mockFn, 3)
+    func()
+    setTimeout(() => {
+      func()
+    }, 2000)
+    setTimeout(() => {
+      mockFn2()
+    }, 4000)
+    jest.runAllTimers()
+    expect(mockFn).toHaveBeenCalledOnce()
+    expect(mockFn2).toHaveBeenCalledBefore(mockFn)
   })
 })
--- test/t_and_d/throttle.test.js
+++ test/t_and_d/throttle.test.js
@@ -1,7 +1,28 @@
+const throttle = require('../../src/t_and_d/throttle.js')
 
-const  throttle = require ('../../src/t_and_d/throttle.js')
-describe('throttle test',()=>{
-  test("pass", () => {
-    // test code
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+describe('throttle test', () => {
+  test("节流函数在一定时间内只会执行一次", () => {
+    const mockFn = jest.fn(arg => null)
+    const func = throttle(mockFn, 3)
+    func()
+    func()
+    jest.runAllTimers()
+    expect(mockFn).toHaveBeenCalledOnce()
+  })
+
+  test("节流函数在超出规定时间之后再次调用会再次执行", () => {
+    const mockFn = jest.fn(arg => null)
+    const func = throttle(mockFn, 3)
+    func()
+    func()
+    setTimeout(()=>{
+      func()
+    },5000)
+    jest.runAllTimers()
+    expect(mockFn).toHaveBeenCalledTimes(2)
   })
 })
